58d2d2865441c9f7ee0a7079da4f7096d92c5cfd
Sebastian <sebascova18@gmail.com>
2026-01-09 15:00:46 -0500

    Add HTTP adapter module

---
+++ internal/adapter/HTTP/routes.go
@@ -0,0 +1,15 @@
+package http
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+
+func RegisterRoutes(r *gin.Engine) {
+  
+	r.GET("/health",func(ctx *gin.Context){
+		ctx.JSON(200,gin.H{
+			"message":"server running",
+		})
+	})
+}
