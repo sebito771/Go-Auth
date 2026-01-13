f294646ecff06d4c8d9b277ebf89e87030d6e595
Sebastian <sebascova18@gmail.com>
2026-01-12 20:12:40 -0500

    feat: create auth handler to register new user

---
+++ internal/adapter/HTTP/handler/auth_handler.go
@@ -0,0 +1,42 @@
+package http
+
+import (
+	"Auth/internal/adapter/http/dto"
+	"Auth/internal/usecases"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+type AuthHandler struct{
+  userRegister *usecases.RegisterUserInput
+}
+
+
+func NewAuthHandler (user *usecases.RegisterUserInput)*AuthHandler{
+   return &AuthHandler{userRegister: user,}
+}
+
+
+func (au *AuthHandler) Register(c *gin.Context){
+	var RegisterReq dto.RegisterRequest
+
+	if err:= c.ShouldBindJSON(&RegisterReq);err != nil{
+		c.JSON(http.StatusBadRequest,gin.H{"error":"invalid request"})
+		return
+	}
+
+	if err := au.userRegister.Execute(RegisterReq.Email,RegisterReq.Password); err != nil{
+		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+	}
+
+	
+	c.JSON(http.StatusCreated,gin.H{"message":"user registered"})
+}
+
+	
+ 
+
+
+
