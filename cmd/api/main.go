aa2a7bc3f263036166351da176f03ba6e48d7cbf
Sebastian <sebascova18@gmail.com>
2026-01-14 08:36:37 -0500

    refactor: remove premature dependency injection in routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	
-	 "Auth/internal/adapters/transport"
+	 
 	"github.com/gin-gonic/gin"
 )
 
 
 func main(){
   r:= gin.Default() 
-  transport.RegisterRoutes(r)
+  
   r.Run(":8000")
 }
