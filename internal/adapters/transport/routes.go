467d3344fdaa2c9f4872eb24675aeed83dc8ce3a
Sebastian <sebascova18@gmail.com>
2026-01-14 11:08:37 -0500

    feat(adapters/transport): add register endpoint

--- internal/adapters/transport/routes.go
+++ internal/adapters/transport/routes.go
@@ -2,13 +2,21 @@ package transport
 
 import (
 	"github.com/gin-gonic/gin"
+	handlers "Auth/internal/adapters/transport/handlers"
 	
 )
 
 
-func RegisterRoutes(r *gin.Engine) {
+func RegisterRoutes(r *gin.Engine, h *handlers.AuthHandler) {
 
-  
+	// create auth group
+    
+	auth:= r.Group("/auth")
+	{
+       auth.POST("/register",h.Register)
+	}
+	// health check route
+ 
 	r.GET("/health",func(ctx *gin.Context){
 		ctx.JSON(200,gin.H{
 			"message":"server running",
