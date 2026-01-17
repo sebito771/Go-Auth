181c8b16e6f175a4172d2b1f3710717d7226b208
Sebastian <sebascova18@gmail.com>
2026-01-16 21:50:07 -0500

    refactor(main): apply dependency injection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-	
-	 
-	"github.com/gin-gonic/gin"
+    "Auth/internal/adapters/repository"
+    "Auth/internal/adapters/security"
+    "Auth/internal/adapters/transport"
+    handlers "Auth/internal/adapters/transport/handlers"
+    "Auth/internal/usecases"
+    
+    "github.com/gin-gonic/gin"
 )
 
-
 func main(){
-  r:= gin.Default() 
+  //adapters
+  repo:= repository.NewMemoryStruct()
+  hasher:= security.BcryptStruct{}
+  //use cases
+  register:= usecases.NewRegisterUser(repo,&hasher)
+  //handler
+  handl:= handlers.NewAuthHandler(register)
+   
+  //gin init
+  r:= gin.Default()
+
+  //router
+  transport.RegisterRoutes(r, handl)
   
   r.Run(":8000")
 }
