79c16215cb916cc00ea0afe02329990f22f22dc6
Sebastian <sebascova18@gmail.com>
2026-01-22 09:48:31 -0500

    feat(usecases) integrate TokenGenerator in LoginUseCase and return string token

--- internal/usecases/login_user.go
+++ internal/usecases/login_user.go
@@ -3,34 +3,40 @@ package usecases
 import (
 	"Auth/internal/ports"
 	"errors"
-	"Auth/internal/domain/user"
 )
 
 type LoginUser struct{
 	repo ports.UserRepository
 	hasher ports.PassWordHaser
+	tokenGen ports.TokenGenerator
 }
 
 var invalidCredentials = errors.New("invalid credentials")
 
-func NewLoginUser(repo ports.UserRepository , hasher ports.PassWordHaser) *LoginUser{
-	return &LoginUser{repo: repo, hasher: hasher}
+func NewLoginUser(repo ports.UserRepository , hasher ports.PassWordHaser,token ports.TokenGenerator) *LoginUser{
+	return &LoginUser{repo: repo, hasher: hasher,tokenGen: token}
 }
 
 
 
-func (l *LoginUser)Auth(email string, password string)(*user.User,error){
+func (l *LoginUser)Auth(email string, password string)(string,error){
 
 	existingEmail,err:= l.repo.FindByEmail(email)
     
 	 if err != nil{
-		return nil , invalidCredentials
+		return "" , invalidCredentials
 	 }
 
 	 hash:= existingEmail.Password()
 	 err= l.hasher.Compare(password,hash)
 	 if err != nil{
-		return nil , invalidCredentials
+		return "" , invalidCredentials
 	 }
-	 return existingEmail , nil
+
+	 token,err:= l.tokenGen.GetToken(existingEmail)
+	 if err != nil{
+		return "" , err
+	 }
+
+	 return token , nil
 }
