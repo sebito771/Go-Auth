adbc0948897e8277e225700c22d757fb343a0c10
Sebastian <sebascova18@gmail.com>
2026-01-22 10:00:08 -0500

    feat(cmd): load .env and implements JWT token in login

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
-    "Auth/internal/adapters/repository"
-    "Auth/internal/adapters/security"
-    "Auth/internal/adapters/transport"
-    handlers "Auth/internal/adapters/transport/handlers"
-    "Auth/internal/usecases"
-    
-    "github.com/gin-gonic/gin"
+	"Auth/internal/adapters/repository"
+	"Auth/internal/adapters/security"
+	"Auth/internal/adapters/transport"
+	handlers "Auth/internal/adapters/transport/handlers"
+	"Auth/internal/usecases"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main(){
+    err:= godotenv.Load(); if err != nil{
+	 log.Fatalf("Error loading .env file: %v", err) 
+	 }
+
+     tokenKey:= os.Getenv("TOKEN_PASSWORD")
   //adapters
   repo:= repository.NewMemoryStruct()
   hasher:= security.BcryptStruct{}
+  tokenGen:= security.NewJwtAdapter(tokenKey)
   //use cases
   register:= usecases.NewRegisterUser(repo,&hasher)
-  login:= usecases.NewLoginUser(repo,&hasher)
+  login:= usecases.NewLoginUser(repo,&hasher,tokenGen)
   //handler
   handl:= handlers.NewAuthHandler(register,login)
    
