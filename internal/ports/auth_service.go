c1d9096f64e33eda33a091cd74803fe6d5bc91ea
Sebastian <sebascova18@gmail.com>
2026-01-22 10:53:02 -0500

    feat(ports): add Token validator interfaces

--- internal/ports/auth_service.go
+++ internal/ports/auth_service.go
@@ -5,3 +5,16 @@ import "Auth/internal/domain/user"
 type TokenGenerator interface {
 	GetToken(user *user.User)(string,error)
 }
+
+//token claims
+
+type TokenClaims struct{
+	UserdID int64
+	Email string
+	Role string
+}
+
+
+type TokenValidator interface{
+	ValidateToken(tokenstr string)(TokenClaims,error)
+}
