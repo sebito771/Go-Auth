f9a89069997b2596c306eb7b03538faacc92c8ad
Sebastian <sebascova18@gmail.com>
2026-01-23 17:59:39 -0500

    feat(ports): add FindById method in Repository interface

--- internal/ports/user_repository.go
+++ internal/ports/user_repository.go
@@ -5,6 +5,7 @@ import "Auth/internal/domain/user"
 type UserRepository interface {
 	Save(user *user.User) error
 	FindByEmail(email string) (*user.User, error)
+	FindById(id int64) (*user.User,error)
 }
 
 
