d13c37707e02602b1ccc0436946f1151111df63f
Sebastian <sebascova18@gmail.com>
2026-01-25 20:29:04 -0500

    refactor(cmd/api): inject middleware in router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"Auth/internal/adapters/security"
 	"Auth/internal/adapters/transport"
 	handlers "Auth/internal/adapters/transport/handlers"
+	"Auth/internal/adapters/transport/middlewares"
 	"Auth/internal/usecases"
 	"log"
 	"os"
@@ -18,22 +19,26 @@ func main(){
 	 log.Fatalf("Error loading .env file: %v", err) 
 	 }
 
-     tokenKey:= os.Getenv("TOKEN_PASSWORD")
+    tokenKey:= os.Getenv("TOKEN_PASSWORD")
   //adapters
   repo:= repository.NewMemoryStruct()
   hasher:= security.BcryptStruct{}
   tokenGen:= security.NewJwtAdapter(tokenKey)
+  
   //use cases
   register:= usecases.NewRegisterUser(repo,&hasher)
   login:= usecases.NewLoginUser(repo,&hasher,tokenGen)
+  profile:= usecases.NewProfilUser(repo)
   //handler
-  handl:= handlers.NewAuthHandler(register,login)
+  handl:= handlers.NewAuthHandler(register,login,profile)
+  //middlewares
+  middl:= middlewares.NewAuhtMiddleWare(tokenGen)
    
   //gin init
   r:= gin.Default()
 
   //router
-  transport.RegisterRoutes(r, handl)
+  transport.RegisterRoutes(r, handl,*middl)
   
   r.Run(":8000")
 }
