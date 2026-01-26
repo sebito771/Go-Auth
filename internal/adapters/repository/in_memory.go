d74e6a598a539ea27d10d9f27736883e66d1a3de
Sebastian <sebascova18@gmail.com>
2026-01-25 20:21:37 -0500

    fix(adapters/repository): ensure user indexing by ID in memory store

--- internal/adapters/repository/in_memory.go
+++ internal/adapters/repository/in_memory.go
@@ -10,13 +10,14 @@ import (
 
 type InMemoryStruct struct{
 	users map[string]*user.User
+   usersByid map[string]*user.User
 	mu sync.RWMutex
 }
 
 var ErrorNotFound= errors.New("user not found")
 
 func NewMemoryStruct()ports.UserRepository{
-	return &InMemoryStruct{users: make(map[string]*user.User),}
+	return &InMemoryStruct{users: make(map[string]*user.User),usersByid: make(map[string]*user.User)}
 }
 
 
@@ -24,11 +25,12 @@ func (in *InMemoryStruct) Save(user *user.User)error{
    in.mu.Lock()
    defer in.mu.Unlock()
 
-
-   in.users[user.Email()]= user
    if user.GetId()==0{
       user.SetId(int64(len(in.users)+1))
    }
+   in.users[user.Email()]= user
+   in.usersByid[strconv.FormatInt(user.GetId(),10)]= user
+  
    return nil
 }
 
@@ -46,7 +48,7 @@ func (in *InMemoryStruct) FindByEmail(email string)(*user.User,error){
 func (in *InMemoryStruct) FindById(id int64)(*user.User,error){
    in.mu.RLock()
    defer in.mu.RUnlock()
-   user,ok:= in.users[strconv.FormatInt(id, 10)]
+   user,ok:= in.usersByid[strconv.FormatInt(id, 10)]
    if !ok{
       return nil,ErrorNotFound
    }
