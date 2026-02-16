234da9720e8c14eed646607d04fb9a64f7ea442f
Sebastian <sebascova18@gmail.com>
2026-02-15 19:51:59 -0500

    feat(security): add black list adapter in memory

---
+++ internal/adapters/security/black_list.go
@@ -0,0 +1,58 @@
+package security
+
+import (
+	"sync"
+	"time"
+)
+
+type BlackList struct {
+	store map[string]time.Time
+	MU sync.RWMutex
+}
+
+func NewBlackList() *BlackList {
+	bl:= &BlackList{store: make(map[string]time.Time)}
+	go bl.cleanUp()
+	return bl
+}
+
+func (bl *BlackList) Add(token string, ttl int64) error {
+	bl.MU.Lock()
+	defer bl.MU.Unlock()
+	expiredAt:= time.Now().Add(time.Duration(ttl) * time.Second)
+	bl.store[token] = expiredAt
+	return nil
+}
+
+func (bl *BlackList) IsBlackListed(token string) (bool, error) {
+	bl.MU.RLock()
+	expiresAt, exist := bl.store[token]
+	if !exist {
+		return false, nil
+	}
+	bl.MU.RUnlock()
+
+	 bl.MU.Lock()
+	defer bl.MU.Unlock()
+	if time.Now().After(expiresAt) {
+		delete(bl.store, token)
+		return false, nil
+	}
+	return true, nil
+}
+
+func(bl *BlackList) cleanUp(){
+	bl.MU.Lock()
+	defer bl.MU.Unlock()
+	ticker:= time.NewTicker(time.Minute)
+
+  for range ticker.C{
+    now:= time.Now()
+	for token, expiresAt:= range bl.store{
+		if now.After(expiresAt){
+			delete(bl.store,token)
+		}
+	}
+  }
+
+}
